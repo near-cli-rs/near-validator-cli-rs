e905ba4fef7f69aed46dd4716d039fb4ad36afeb
FroVolod <frol_off@meta.ua>
2023-06-21 18:06:07 +0300

    moved display_current_validators_info()

--- src/validators/block_id/mod.rs
+++ src/validators/block_id/mod.rs
@@ -1,11 +1,20 @@
-use near_primitives::types::{BlockId, EpochReference};
+use color_eyre::eyre::Context;
+use prettytable::Table;
+
+use near_jsonrpc_client::methods::{
+    validators::RpcValidatorRequest, EXPERIMENTAL_genesis_config::RpcGenesisConfigRequest,
+    EXPERIMENTAL_protocol_config::RpcProtocolConfigRequest,
+};
+use near_primitives::types::{BlockId, BlockReference, EpochReference, Finality};
+
+use near_cli_rs::common::JsonRpcClientExt;
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(input_context = super::network_view_at_block::NetworkViewAtBlockArgsContext)]
 #[interactive_clap(output_context = AtBlockHeightContext)]
 pub struct AtBlockHeight {
     /// Type the block ID height:
-    block_id_height: near_primitives::types::BlockHeight,
+    block_height: near_primitives::types::BlockHeight,
 }
 
 #[derive(Debug, Clone)]
@@ -16,8 +25,8 @@ impl AtBlockHeightContext {
         previous_context: super::network_view_at_block::NetworkViewAtBlockArgsContext,
         scope: &<AtBlockHeight as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
     ) -> color_eyre::eyre::Result<Self> {
-        let epoch_reference = EpochReference::BlockId(BlockId::Height(scope.block_id_height));
-        super::epoch_id::latest::display_current_validators_info(
+        let epoch_reference = EpochReference::BlockId(BlockId::Height(scope.block_height));
+        display_current_validators_info(
             epoch_reference,
             &previous_context.network_config,
         )?;
@@ -30,7 +39,7 @@ impl AtBlockHeightContext {
 #[interactive_clap(output_context = BlockIdHashContext)]
 pub struct BlockIdHash {
     /// Type the block ID hash:
-    block_id_hash: near_cli_rs::types::crypto_hash::CryptoHash,
+    block_hash: near_cli_rs::types::crypto_hash::CryptoHash,
 }
 
 #[derive(Debug, Clone)]
@@ -41,11 +50,86 @@ impl BlockIdHashContext {
         previous_context: super::network_view_at_block::NetworkViewAtBlockArgsContext,
         scope: &<BlockIdHash as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
     ) -> color_eyre::eyre::Result<Self> {
-        let epoch_reference = EpochReference::BlockId(BlockId::Hash(scope.block_id_hash.into()));
-        super::epoch_id::latest::display_current_validators_info(
-            epoch_reference,
-            &previous_context.network_config,
-        )?;
+        let epoch_reference = EpochReference::BlockId(BlockId::Hash(scope.block_hash.into()));
+        display_current_validators_info(epoch_reference, &previous_context.network_config)?;
         Ok(Self)
     }
+}
+
+pub fn display_current_validators_info(
+    epoch_reference: near_primitives::types::EpochReference,
+    network_config: &near_cli_rs::config::NetworkConfig,
+) -> crate::CliResult {
+    let block_reference = match &epoch_reference {
+        EpochReference::Latest => BlockReference::Finality(Finality::Final),
+        EpochReference::BlockId(block_id) => BlockReference::BlockId(block_id.clone()),
+        _ => {
+            return Err(color_eyre::eyre::ErrReport::msg(
+                "BlockReference: incorrect value entered",
+            ))
+        }
+    };
+
+    let json_rpc_client = network_config.json_rpc_client();
+
+    let mut current_validators = json_rpc_client
+        .blocking_call(&RpcValidatorRequest { epoch_reference })
+        .wrap_err("Failed to get epoch validators information request.")?
+        .current_validators;
+    current_validators.sort_by(|a, b| b.stake.cmp(&a.stake));
+
+    let genesis_config = json_rpc_client
+        .blocking_call(&RpcGenesisConfigRequest)
+        .wrap_err("Failed to get genesis config.")?;
+
+    let protocol_config = json_rpc_client
+        .blocking_call(&RpcProtocolConfigRequest { block_reference })
+        .wrap_err("Failed to get protocol config.")?;
+
+    let max_number_of_seats = protocol_config.num_block_producer_seats
+        + protocol_config
+            .avg_hidden_validator_seats_per_shard
+            .iter()
+            .sum::<u64>();
+    println!(
+        "Validators (total: {}, seat price: {}",
+        current_validators.len(),
+        crate::common::find_seat_price(
+            current_validators
+                .iter()
+                .cloned()
+                .map(crate::common::CurrentOrNextValidatorInfoOrProposalsTable::from)
+                .collect(),
+            max_number_of_seats,
+            genesis_config.minimum_stake_ratio,
+            protocol_config.protocol_version
+        )?
+    );
+
+    let mut table = Table::new();
+    table.set_titles(prettytable::row![Fg=>"Validator Id", "Stake", "Online", "Blocks produced", "Blocks expected", "Chunks produced", "Chunks expected"]);
+
+    for validator in &current_validators {
+        let online = if validator.num_expected_blocks + validator.num_expected_chunks == 0 {
+            "NaN".to_string()
+        } else {
+            format!(
+                "{:>6.2} %",
+                ((validator.num_produced_blocks + validator.num_produced_chunks) * 100) as f64
+                    / (validator.num_expected_blocks + validator.num_expected_chunks) as f64
+            )
+        };
+        table.add_row(prettytable::row![
+            validator.account_id,
+            near_cli_rs::common::NearBalance::from_yoctonear(validator.stake),
+            online,
+            validator.num_produced_blocks,
+            validator.num_expected_blocks,
+            validator.num_produced_chunks,
+            validator.num_expected_chunks
+        ]);
+    }
+    table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.printstd();
+    Ok(())
 }
