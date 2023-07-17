fb0b398019d164e4f40f9f5a13be900e26f8d68b
FroVolod <frol_off@meta.ua>
2023-07-17 12:46:16 +0300

    added UnstakeProposal

--- src/staking/mod.rs
+++ src/staking/mod.rs
@@ -1,6 +1,7 @@
 use strum::{EnumDiscriminants, EnumIter, EnumMessage};
 
-mod proposal;
+mod stake_proposal;
+mod unstake_proposal;
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(context = near_cli_rs::GlobalContext)]
@@ -14,9 +15,16 @@ pub struct Staking {
 #[strum_discriminants(derive(EnumMessage, EnumIter))]
 /// What are you up to? (select one of the options with the up-down arrows on your keyboard and press Enter)
 pub enum StakingCommand {
-    #[strum_discriminants(strum(message = "proposal    -   Add a new stake proposal"))]
-    /// Add a new stake proposal
-    Proposal(self::proposal::StakeProposal),
+    #[strum_discriminants(strum(
+        message = "stake-proposal      -   To stake NEAR directly without a staking pool"
+    ))]
+    /// To stake NEAR directly without a staking pool
+    StakeProposal(self::stake_proposal::StakeProposal),
+    #[strum_discriminants(strum(
+        message = "unstake-proposal    -   To unstake NEAR directly without a staking pool"
+    ))]
+    /// To unstake NEAR directly without a staking pool
+    UnstakeProposal(self::unstake_proposal::UnstakeProposal),
     #[strum_discriminants(strum(message = "current"))]
     /// Lookup validators for given epoch
     Current,
